5c60469318700138a9987fb7d147439a37ee3787
cdsantan <53973786+cdsantan@users.noreply.github.com>
2021-01-20 15:47:02 -0500

    Add files via upload

---
+++ v7p3/PacletInfo.m
@@ -0,0 +1,25 @@
+(* ::Package:: *)
+
+Paclet[
+	"Name" ->  "Mechanisms",
+	"Version" ->  "0.7.3",
+	"Creator" ->  "cdsantan@syr.edu",
+	"Description" ->  "Mechanisms package",
+	"WolframVersion" ->  "11+",
+	"Extensions"  ->  {
+		{"Kernel", 
+			"Root" -> "Kernel", 
+			"Context" -> {"Mechanisms`"}
+		},
+		{"Resource",
+			"Root" -> "Resources", 
+			"Resources" -> {
+				{"CrinkledPaper","Textures/crinkled-paper.jpg"}
+			}
+		},
+		{"Documentation",
+			"Language"->"English",
+			"MainPage" -> "Guides/Mechanisms"
+		}
+	}
+]
